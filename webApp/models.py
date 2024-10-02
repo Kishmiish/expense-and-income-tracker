a07b7f09e59695d2d31ed988bc9f94d5fdd8acb0
Kishmiish <sma.smh.2771@gmail.com>
2024-10-02 22:38:01 +0330

    feat(models): create income and token models

--- webApp/models.py
+++ webApp/models.py
@@ -1,6 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Token(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    Token = models.CharField(max_length=32)
 
 
 class Expense(models.Model):
@@ -8,3 +14,12 @@ class Expense(models.Model):
     description = models.CharField(max_length=255)
     date = models.DateTimeField()
     amount = models.BigIntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+
+
+class Income(models.Model):
+    text = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
