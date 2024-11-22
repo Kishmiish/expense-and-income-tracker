7fcab80ed7264ab7d7449e712615e1a65e3b1227
Kishmiish <sma.smh.2771@gmail.com>
2024-11-23 03:28:02 +0330

    fix: moved view budget to root directory

--- webApp/urls.py
+++ webApp/urls.py
@@ -4,7 +4,7 @@ from webApp import views
 urlpatterns = [
     re_path(r'^submit/expense/$', views.submit_expense),
     re_path(r'^submit/income/$', views.submit_income),
-    re_path(r'^budget/$', views.view_budget),
+    re_path(r'^/$', views.view_budget),
     re_path(r'^expenses/all/$', views.all_expenses),
     re_path(r'^incomes/all/$', views.all_incomes),
 ]
